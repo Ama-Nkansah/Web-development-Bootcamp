b1a5bce81203986d83eec3ce77d17c79bae7a3c9
Ama Nkansah <amankansahstate@gmail.com>
2025-01-20 15:58:23 +0000

    added my first http request

--- Backend/3.1-Express-server/index.js
+++ Backend/3.1-Express-server/index.js
@@ -2,6 +2,12 @@ import express from "express";
 const app= express();
 const port =3000
 
+//https request
+ app.get('/',(req,res)=>{
+ res.send('Hello world');
+ });
+
+ 
 app.listen(port,()=>{
   console.log(`server running on port ${port}.`);
 });
