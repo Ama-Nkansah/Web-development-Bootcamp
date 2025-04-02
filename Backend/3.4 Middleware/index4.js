3c1a69dbb15d8815ccadf008af37ff0dd12375f9
Ama Nkansah <amankansahstate@gmail.com>
2025-04-02 02:14:11 +0100

    band generator project(body-parser & middleware

--- Backend/3.4 Middleware/index4.js
+++ Backend/3.4 Middleware/index4.js
@@ -1,7 +1,31 @@
 import express from "express";
+import bodyParser from 'body-parser';
+import { dirname } from "path"
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+function logger(req, res, next) {
+  console.log("Request Method: ", req.method);
+  console.log("Request URL: ", req.url);
+  next();
+}
+
+app.use(logger);
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.send("Form submitted successfully!");
+});
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
