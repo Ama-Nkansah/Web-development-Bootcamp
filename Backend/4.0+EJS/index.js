3925a4873d63700f313e3f5f2aa7b7139a49145f
Ama Nkansah <amankansahstate@gmail.com>
2025-04-03 17:03:37 +0100

    created my first EJS project

---
+++ Backend/4.0+EJS/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+
+const today= new Date("2025-04-03");
+const day = today.getDay();
+const dayNames = ["Sunday", "Monday", "Tuesday",'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+ let message;
+ let answered_day;
+
+if (day>=1 && day<=4){
+  answered_day = "weekday";
+ message =`${answered_day} , lets get some work done`;
+}
+else{
+   answered_day = "weekend";
+message = `${answered_day} , it's time to have fun`;
+}
+
+app.get('/', (req, res) => {
+  res.render('index', { outcome: message }); 
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
