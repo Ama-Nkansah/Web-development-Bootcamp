8d0f9161accb5d7e3a0ddc6788b93fbeb8916bbc
Ama Nkansah <amankansahstate@gmail.com>
2025-05-20 19:57:27 +0100

    api auth & athZ

---
+++ Backend/5.4+API+Authentication/index.js
@@ -0,0 +1,83 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com/";
+
+const yourUsername = "ama";
+const yourPassword = "348";
+const yourAPIKey = "1234";
+const yourBearerToken = "5678";
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "API Response." });
+});
+
+app.get("/noAuth", async (req, res) => {
+  try{
+  const result  = await axios.get(API_URL +"/random")
+  res.render(index.ejs, {content:JSON.stringify(result.data)})
+}catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.get("/basicAuth", async (req, res) => {
+  try {
+    const result = await axios.get(`${API_URL}/all?page=2`, {
+      auth: {
+        username: "ama",
+        password: "348",
+      },
+    });
+    res.send(result.data); 
+  } catch (error) {
+    res.status(404).send(error.message); 
+  }
+});
+
+
+
+app.get("/apiKey", async (req, res) => {
+  try{
+  const result  = await axios.get(`${API_URL} /filter`,{
+  params: {
+        score: 5,
+        apiKey:1234,
+      },
+  });
+  res.render(index.ejs, {content:JSON.stringify(result.data)})
+}catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.get("/bearerToken", async (req, res) => {
+  try{
+  const result  = await axios.get(`${API_URL} /filter`,{
+  params: {
+        score: 5,
+        apiKey:1234,
+      },
+  });
+  res.render(index.ejs, {content:JSON.stringify(result.data)})
+}catch (error) {
+    res.status(404).send(error.message);
+  }
+  //TODO 5: Write your code here to hit up the /secrets/{id} endpoint
+  //and get the secret with id of 42
+  //HINT: This is how you can use axios to do bearer token auth:
+  // https://stackoverflow.com/a/52645402
+  /*
+  axios.get(URL, {
+    headers: { 
+      Authorization: `Bearer <YOUR TOKEN HERE>` 
+    },
+  });
+  */
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
